0a59d63aec439d884916a1b7b14be431e05467df
Eric Abell <ericabell@users.noreply.github.com>
2017-09-19 13:32:10 -0500

    Basic requirements complete

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,8 +1,53 @@
 package com.example;
 
+import com.example.common.Month;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // display all the English month names
+        for( Month m: Month.values() ) {
+            System.out.println(m.getEnglishName());
+        }
+
+        System.out.println("Please choose a month:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        ArrayList<Month> months = new ArrayList<Month>();
+
+        switch(input) {
+            case "January":
+                months.add(Month.JANUARY);
+            case "February":
+                months.add(Month.FEBRUARY);
+            case "March":
+                months.add(Month.MARCH);
+            case "April":
+                months.add(Month.APRIL);
+            case "May":
+                months.add(Month.MAY);
+            case "June":
+                months.add(Month.JUNE);
+            case "July":
+                months.add(Month.JULY);
+            case "August":
+                months.add(Month.AUGUST);
+            case "September":
+                months.add(Month.SEPTEMBER);
+            case "October":
+                months.add(Month.OCTOBER);
+            case "November":
+                months.add(Month.NOVEMBER);
+            case "December":
+                months.add(Month.DECEMBER);
+        }
+        System.out.println(months);
     }
 }
